ecd9f3bd5c9547f0e813de5719bbe86e9485751c
yurong <rong.yu@thoughtworks.com>
2020-09-24 16:43:09 +0800

    [yurong] feat: add trainee filed github

--- src/main/java/com/example/demo/domain/Trainee.java
+++ src/main/java/com/example/demo/domain/Trainee.java
@@ -8,6 +8,7 @@ import lombok.*;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
@@ -25,6 +26,8 @@ public class Trainee {
     @NotEmpty(message = ErrorMsg.TRAINEE_EMAIL_NOT_EMPTY)
     @Email(message = ErrorMsg.TRAINEE_EMAIL_FORMAT_NOT_VALID)
     private String email;
+    @NotNull(message = ErrorMsg.TRAINEE_GITHUB_NOT_EMPTY)
+    private String github;
     @NotEmpty(message = ErrorMsg.TRAINEE_ZOOM_ID_NOT_EMPTY)
     private String zoomId;
     @JsonIgnore
--- src/main/java/com/example/demo/exception/ErrorMsg.java
+++ src/main/java/com/example/demo/exception/ErrorMsg.java
@@ -4,6 +4,7 @@ public class ErrorMsg {
     public static final String TRAINEE_NAME_NOT_EMPTY = "学员名字不能为空";
     public static final String TRAINEE_OFFICE_NOT_EMPTY = "学员办公室不能为空";
     public static final String TRAINEE_EMAIL_NOT_EMPTY = "学员办邮箱地址不能为空";
+    public static final String TRAINEE_GITHUB_NOT_EMPTY = "学员办github账号不能为空";
     public static final String TRAINEE_EMAIL_FORMAT_NOT_VALID = "邮箱格式错误";
     public static final String TRAINEE_ZOOM_ID_NOT_EMPTY = "学员zoomId不能为空";
     public static final String TRAINEE_NOT_EXIST = "id为%d的学员不存在";
